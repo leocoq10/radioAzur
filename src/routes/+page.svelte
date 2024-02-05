018f8a833a35ddd11c43dc4eddc42cab763c516b
leocoq10 <aoulileo@gmail.com>
2024-02-05 09:21:30 +0000

    Revert "correction du slider"
    This reverts commit 14440d302f1713c5cfd09c4db45a0632650b5bb0.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-    import Big_Slider from "../components/Big_Slider.svelte"
+    import Slider from "../components/Slider.svelte"
     import LiveBox from "../components/LiveBox.svelte";
     import Programme from "../components/Programme.svelte";
 
 </script>
 
 <main>
-    <Big_Slider/>
+    <Slider/>
     <LiveBox/>
     <Programme/>
 </main>
